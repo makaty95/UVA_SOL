ffa007a46943d3ca4a6160b1da3b5a3000ab353c
mohamed <mohamedmakaty2023@gmail.com>
2023-10-30 00:31:35 +0200

    Adding Solution to UVA 10945 - Mother bear

--- 10945 - Mother bear.cpp
+++ 10945 - Mother bear.cpp
@@ -5,20 +5,18 @@
 	name : mohamed makaty
 	i hope this help :)
 */
+
 #include <bits/stdc++.h>
-#include <unordered_map>
-#include <unordered_set>
 #define ll long long
 #define ld long double
-#define lp(i, a, n) for (ll i = a; i <= (n); i++)
 #define ff first
 #define ss second
 #define all(a) a.begin(), a.end()
 #define endl '\n'
 #define EPS 1e-8
+#define inf INT_MAX
 #define pb push_back
 using namespace std;
-#define SZ 100000003
 void fastio()
 {
   ios_base::sync_with_stdio(false);
@@ -33,8 +31,8 @@ void file()
 #endif
 }
 
-int dx[] = {1, 0, -1, 0, 1, -1, 1, -1};
-int dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
+//int dx[] = {1, 0, -1, 0, 1, -1, 1, -1};
+//int dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
 // bool dequal(double a, double b)
 // {
 //   if(fabs(a-b) <= EPS)
@@ -50,48 +48,55 @@ int dy[] = {0, 1, 0, -1, 1, -1, -1, 1};
 //   return x < n && y < m && x >= 0 && y >= 0;
 // }
 
-bool pl(string s)
+bool pal(string s)
 {
-  int i = 0, j = s.size() - 1;
-  while(i<=j)
+  int n = s.size();
+  for (int i = 0, j = n - 1; i <= j; i++, j--)
   {
     if(s[i] != s[j])
       return 0;
-
-    i++, j--;
   }
   return 1;
 }
-void solve()
+
+string modify(string s)
 {
-  string s;
-  while(getline(cin,s) && s != "DONE")
+  string ans;
+  for(auto c : s)
   {
-    string k;
-    for (int i = 0; i < s.size(); i++)
-    {
-      if(isalpha(s[i]))
-        k.push_back(tolower(s[i]));
-    }
-
-    if(pl(k))
-      cout << "You won't be eaten!" << endl;
-    else
-      cout << "Uh oh.." << endl;
+    if(isalpha(c))
+      ans.pb(tolower(c));
   }
-
+  return ans;
 }
 
-int main() // (:-3<)--<--<
+void solve()
+{
+  
+}
+
+
+
+
+int main() //:-3<)--<--<
 {
 
   file();
   fastio();
-  // int t;
-  // cin >> t;
-  // while(t--)
-  solve();
+
+  string s;
+  while(getline(cin, s) && s != "DONE")
+  {
+    string modif = modify(s);
+    if(pal(modif))
+    {
+      cout << "You won't be eaten!" << endl;
+    }
+    else
+    {
+      cout << "Uh oh.." << endl;
+    }
+  }
+
+  
 }
-
-
-
